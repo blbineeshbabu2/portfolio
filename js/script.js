51277fe1cb7265bb2e7cefa9ba15bc33af65fa26
Bineesh Babu B L <blbineeshbabu2@gmail.com>
2025-02-02 22:36:21 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,3 +22,132 @@ document.addEventListener("DOMContentLoaded", function () {
         loop: true
     });
 });
+
+
+
+
+
+var nameError = document.getElementById('name-error');
+var emailError = document.getElementById('email-error');
+var phoneError = document.getElementById('phone-error');
+var subjectError = document.getElementById('subject-error');
+var messageError = document.getElementById('message-error');
+var submitError = document.getElementById('submit-error');
+
+/*name validation*/
+
+function validateName(){
+    var name = document.getElementById('contact-name').value;
+
+    if(name.match(/[0-9]/)){
+        nameError.innerHTML = 'Name should be characters';
+        return false;
+    }
+
+    if(name.length == 0){
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+/*email validation*/
+
+function validateEmail(){
+    var email = document.getElementById('contact-email').value;
+
+    if(email.length == 0){
+        emailError.innerHTML = 'Email required';
+        return false;
+    }
+
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)){
+        emailError.innerHTML = 'Email invalid'
+        return false;
+    }
+
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+/*Phone number validation*/
+
+function validatePhone(){
+    var phone = document.getElementById('contact-phone').value;
+
+    if(phone.match(/[A-Za-z]/)){
+        phoneError.innerHTML = 'Phone should be numbers';
+        return false;
+    }
+
+    if(phone.length == 0){
+        phoneError.innerHTML = 'Phone number required'
+        return false;
+    }
+
+    if(phone.length !== 10){
+        phoneError.innerHTML = 'Phone should be 10 digits!'
+        return false;
+    }
+
+    if(!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = 'Only should be numbers'
+        return false;
+    }
+
+    phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+/*Subject validation*/
+
+function validateSubject(){
+    var subject = document.getElementById('contact-subject').value;
+
+    if(subject.match(/[0-9]/)){
+        subjectError.innerHTML = 'Subject only contain characters';
+        return false;
+    }
+
+    if(subject.length < 4){
+        subjectError.innerHTML = 'Subject required';
+        return false;
+    }
+
+    subjectError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+/*Message validation*/
+
+function validateMessage(){
+    var message = document.getElementById('contact-message').value;
+    var required = 40;
+    var num = required - message.length;
+
+    if(num > 0){
+        messageError.innerHTML = num + ' more characters required';
+        return false;
+    }
+
+    messageError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+/*Submit button validation*/
+
+function validateForm(){
+    if(!validateName() || !validateEmail() || !validatePhone() || !validateSubject() || !validateMessage()){
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fill form';
+        setTimeout(function(){submitError.style.display = 'none';}, 3000); /*Hide message after 3 seconds*/
+        return false;
+    }
+}
